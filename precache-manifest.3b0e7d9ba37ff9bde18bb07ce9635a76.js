fa5a767586c551c996f8ee439fac3c89edb16dee
KerronKing <king_of_palmiste@hotmail.com>
2020-04-09 17:10:16 -0400

    Updates

---
+++ precache-manifest.3b0e7d9ba37ff9bde18bb07ce9635a76.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a0aa2fd2095206420daaafc0f04700c",
+    "url": "/kerron-king-portfolio/index.html"
+  },
+  {
+    "revision": "593404f21a2e42c3e005",
+    "url": "/kerron-king-portfolio/static/css/main.7a00a020.chunk.css"
+  },
+  {
+    "revision": "eebc70eeed13249acc22",
+    "url": "/kerron-king-portfolio/static/js/2.98388eb9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kerron-king-portfolio/static/js/2.98388eb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "593404f21a2e42c3e005",
+    "url": "/kerron-king-portfolio/static/js/main.5cdabb50.chunk.js"
+  },
+  {
+    "revision": "6fddbceb75dc2362e67f",
+    "url": "/kerron-king-portfolio/static/js/runtime-main.633cad21.js"
+  },
+  {
+    "revision": "0cbfde3f62aa413ecf8980088c97a5d6",
+    "url": "/kerron-king-portfolio/static/media/api-article.0cbfde3f.jpeg"
+  },
+  {
+    "revision": "9a3995842e6f103073dd7c8bc5b0fa18",
+    "url": "/kerron-king-portfolio/static/media/barrier-article.9a399584.jpeg"
+  },
+  {
+    "revision": "1d6f42af2e5c524b86132fb72dcd6a19",
+    "url": "/kerron-king-portfolio/static/media/prime-article.1d6f42af.jpeg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/kerron-king-portfolio/precache-manifest.3b0e7d9ba37ff9bde18bb07ce9635a76.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kerron-king-portfolio/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
