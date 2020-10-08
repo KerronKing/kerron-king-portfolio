24029fe2f902badc0621df3fd4ebc15005cae549
KerronKing <king_of_palmiste@hotmail.com>
2020-10-08 18:45:24 -0400

    style: introduce animations

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Info from './containers/info';
 import ProjectsSection from './containers/projects-section';
-import './App.css';
+import './App.scss';
 
 const App = () => (
   <div className="App">
--- src/components/project.js
+++ src/components/project.js
@@ -1,33 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Slide from 'react-reveal/Slide';
+import Zoom from 'react-reveal/Zoom';
 
 const Project = ({
   title, description, imgSrc, alt, source, live, style,
 }) => (
-  <div className="project-tile" id={style}>
-    <h2>{title}</h2>
-    <img
-      src={imgSrc}
-      alt={alt}
-      height="400"
-      width="400"
-    />
-    <p>{description}</p>
-    <a
-      href={source}
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Source code
-    </a>
-    <a
-      href={live}
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Live demo
-    </a>
-  </div>
+  <Slide left>
+    <div className="project-tile" id={style}>
+      <h2>{title}</h2>
+      <Zoom>
+        <img src={imgSrc} alt={alt} height="400" width="400" />
+      </Zoom>
+      <p>{description}</p>
+      <Zoom left>
+        <a href={source} target="_blank" rel="noopener noreferrer">
+          Source code
+        </a>
+      </Zoom>
+      <Zoom left>
+        <a href={live} target="_blank" rel="noopener noreferrer">
+          Live demo
+        </a>
+      </Zoom>
+    </div>
+  </Slide>
 );
 
 Project.propTypes = {
