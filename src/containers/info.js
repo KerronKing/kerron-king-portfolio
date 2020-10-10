a184a8a5e8e5600eff0a5d7a29af4e45e067de7b
KerronKing <king_of_palmiste@hotmail.com>
2020-10-10 09:11:54 -0400

    chore: add fade in to first tile and edit scss syntax

--- src/containers/info.js
+++ src/containers/info.js
@@ -1,35 +1,38 @@
 import React from 'react';
+import Fade from 'react-reveal/Fade';
 import contacts from '../data/contacts';
 
 const Info = () => (
-  <div id="info">
-    <h1>KERRON KING</h1>
-    <p>
-      I&apos;m a software engineer based in Trinidad and Tobago.&#32;
-      Let&apos;s work together to solve the world&apos;s problems one click at a time.
-    </p>
-    <div className="social">
-      {contacts.map(contact => (
-        <a
-          key={contact.id}
-          href={contact.ref}
-          rel="noopener noreferrer"
-          target="_blank"
-          alt={contact.alt}
-        >
-          <img
-            src={contact.imgSrc}
+  <Fade bottom>
+    <div id="info">
+      <h1>KERRON KING</h1>
+      <p>
+        I&apos;m a software engineer based in Trinidad and Tobago.&#32;
+        Let&apos;s work together to solve the world&apos;s problems one click at a time.
+      </p>
+      <div className="social">
+        {contacts.map(contact => (
+          <a
+            key={contact.id}
+            href={contact.ref}
+            rel="noopener noreferrer"
+            target="_blank"
             alt={contact.alt}
-            height="30"
-            width="30"
-          />
-        </a>
-      ))}
+          >
+            <img
+              src={contact.imgSrc}
+              alt={contact.alt}
+              height="30"
+              width="30"
+            />
+          </a>
+        ))}
+      </div>
+      <a href="mailto:kerronk1989@gmail.com?subject=Profile Follow Up">
+        <button type="submit">Send a message</button>
+      </a>
     </div>
-    <a href="mailto:kerronk1989@gmail.com?subject=Profile Follow Up">
-      <button type="submit">Send a message</button>
-    </a>
-  </div>
+  </Fade>
 );
 
 export default Info;
